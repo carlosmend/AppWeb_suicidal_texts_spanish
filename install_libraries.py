6c8d40544b7f878429d33d7024f57132fe11d576
CarlosMendoza428 <roberto.mendoza@correounivalle.edu.co>
2023-08-11 17:11:32 -0500

    library correction

--- install_libraries.py
+++ install_libraries.py
@@ -1,7 +1,7 @@
 import subprocess
 
-libs=['pip install Flask', 'pip install tensorflow','pip install spacy','pip install scikit-learn==1.2.2'
-'python -m spacy download es_core_news_lg']
+libs=['py -m pip install Flask', 'py -m pip install tensorflow','py -m pip install spacy','py -m pip install scikit-learn==1.2.2',
+      'py -m pip install flask-cors','py -m spacy download es_core_news_lg']
 
 for library in libs:
     subprocess.run(library, shell=True)
